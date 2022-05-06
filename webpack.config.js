ad1be5d2cc5ec7efdb876cf183414cbb9562b015
77131202@qq.com <77131202@qq.com>
2022-05-06 11:47:11 +0800

    update packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const webpack = require('webpack');
+// const webpack = require('webpack');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const ROOT = path.resolve(__dirname, 'src');
